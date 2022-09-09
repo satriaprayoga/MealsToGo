1dcdd655bd13d998e886917ef26ff87c4414051d
satriaprayoga <satria.prayoga@gmail.com>
2022-09-10 00:31:06 +0700

    spacer

--- src/features/restaurants/components/restaurant-info-card.component..js
+++ src/features/restaurants/components/restaurant-info-card.component..js
@@ -1,50 +1,52 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import { Card } from 'react-native-paper';
 import { SvgXml } from 'react-native-svg';
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
 import styled from 'styled-components/native';
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.bg.primary};
-  padding: ${(props) => props.theme.space[3]}
-`;
-
-const Title = styled(Text)`
-  font-family:${(props) => props.theme.fonts.heading};
-  font-size:${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary}; 
 `;
 
 const Address = styled(Text)`
-font-family:${(props) => props.theme.fonts.body};
-font-size:${(props) => props.theme.fontSizes.caption};
- 
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
 const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]}
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const Rating = styled.View`
   flex-direction: row;
   padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]}
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 const Section = styled.View`
   flex-direction: row;
   align-items: center;
 `;
+
 const SectionEnd = styled.View`
-  flex:1;
-  flex-direction:row;
-  justify-content:flex-end;
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 const Open = styled(SvgXml)`
@@ -54,12 +56,12 @@ const Open = styled(SvgXml)`
 const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
-    photos = ['https://www.foodiesfeed.com/wp-content/uploads/2020/03/close-up-of-a-juicy-beef-burger-in-a-restaurant-1024x683.jpg'],
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/lodging-71.png",
+    photos = ['https://www.foodiesfeed.com/wp-content/uploads/2015/06/chicken-steak-with-egg-spinach-broccoli-avocado-and-carrot-1024x1024.jpg'],
     address = "100 some street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporary
+    isClosedTemporary = true
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -75,7 +77,15 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
             ))}
           </Rating>
           <SectionEnd>
+            {isClosedTemporary && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer variant="left.large" />
             {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer variant="left.large" />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
         </Section>
         <Address>{address}</Address>
