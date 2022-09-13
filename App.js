b7c9e81906e77436e07786e879d381278cdfc994
satriaprayoga <satria.prayoga@gmail.com>
2022-09-13 23:30:53 +0700

    service

--- App.js
+++ App.js
@@ -5,6 +5,37 @@ import { theme } from './src/infrastructure/theme';
 
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
+import { SafeArea } from './src/components/utility/safe-area.component';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Settings = () => (
+  <SafeArea><Text>Settings</Text></SafeArea>
+);
+const Maps = () => (
+  <SafeArea><Text>Maps</Text></SafeArea>
+);
+const Tab=createBottomTabNavigator();
+
+const TAB_ICON={
+  Restaurants:"md-restaurant",
+  Maps:"md-map",
+  Settings:"md-settings"
+}
+
+const createScreenOptions=({route})=>{
+  const iconName=TAB_ICON[route.name];
+  return {
+    tabBarIcon:({size,color})=>(
+      <Ionicons name={iconName} size={size} color={color}/>
+    ),
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  }
+  
+};
 
 export default function App() {
 
@@ -23,7 +54,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator
+          screenOptions={createScreenOptions}
+          >
+            <Tab.Screen name='Restaurants' component={RestaurantsScreen}/>
+            <Tab.Screen name='Settings' component={Settings}/>
+            <Tab.Screen name='Maps' component={Maps}/>
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -3,12 +3,9 @@ import {SafeAreaView, StatusBar, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import styled from 'styled-components/native';
 import { Spacer } from '../../../components/spacer/spacer.component';
+import { SafeArea } from '../../../components/utility/safe-area.component';
 import RestaurantInfoCard from '../components/restaurant-info-card.component.';
 
-const SafeArea=styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
 
 const SearchContainer=styled.View`
   padding: ${(props)=>props.theme.space[3]}
@@ -19,7 +16,6 @@ const RestaurantList = styled(FlatList).attrs({
     padding:16
   }
 })``;
-
 
 
 const RestaurantsScreen = () => {
